2bf8efa5ac15c10908ec183c44df310f21fab354
Emre Esen <emreesnn17@gmail.com>
2025-02-25 22:45:53 +0300

    Üçgen çizme projesi tamamlandı.

---
+++ triangle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int size = GetTriangleSize();
+        DrawTriangle(size);
+    }
+
+    static int GetTriangleSize()
+    {
+        Console.Write("Üçgenin boyutunu girin: ");
+        int size;
+        while (!int.TryParse(Console.ReadLine(), out size) || size <= 0)
+        {
+            Console.Write("Geçerli bir sayı girin: ");
+        }
+        return size;
+    }
+
+    static void DrawTriangle(int size)
+    {
+        for (int i = 1; i <= size; i++)
+        {
+            Console.WriteLine(new string('*', i));
+        }
+    }
+}
