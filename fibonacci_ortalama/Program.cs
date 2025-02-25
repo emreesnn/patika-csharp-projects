5cfac6d34f3dc9a3ebb47342d1cb7c4afbccc094
Emre Esen <emreesnn17@gmail.com>
2025-02-25 18:44:46 +0300

    Fibonacci projesi tamamlandı.

---
+++ fibonacci_ortalama/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Derinlik belirleyiniz: ");
+        if (int.TryParse(Console.ReadLine(), out int derinlik) && derinlik > 0)
+        {
+            int[] fib = new int[derinlik];
+            Fibonacci(derinlik, fib);
+
+            Console.WriteLine("Fibonacci Serisi:");
+            foreach (int num in fib)
+            {
+                Console.Write(num + " ");
+            }
+
+            double ortalama = OrtalamaHesapla(fib);
+            Console.WriteLine($"\nOrtalama: {ortalama}");
+        }
+        else
+        {
+            Console.WriteLine("Lütfen geçerli bir pozitif tamsayı giriniz.");
+        }
+
+        Console.Read();
+    }
+
+    static void Fibonacci(int n, int[] fib)
+    {
+        if (n > 0) fib[0] = 0;
+        if (n > 1) fib[1] = 1;
+
+        for (int i = 2; i < n; i++)
+        {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+    }
+
+    static double OrtalamaHesapla(int[] dizi)
+    {
+        double toplam = 0;
+        foreach (int sayi in dizi)
+        {
+            toplam += sayi;
+        }
+        return dizi.Length > 0 ? toplam / dizi.Length : 0;
+    }
+}
