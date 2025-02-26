397c01ceef884dcd24501b3f25f0b38aba9c19b4
Emre Esen <emreesnn17@gmail.com>
2025-02-26 04:27:01 +0300

    Karakter Tersten Yazdırma projesi tamamlandı.

---
+++ KarakterTerstenYazdırma/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace KarakterTerstenYazdirma;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = GetUserInput();
+        Console.WriteLine(ReverseString(text)); 
+    }
+
+    static string GetUserInput()
+    {
+        Console.WriteLine("String bir ifade giriniz: ");
+        string input = Console.ReadLine();
+        return input;
+    }
+
+    static string ReverseString(string input)
+    {
+        string[] splittedText = input.Split(' ');
+        string reversedText = "";
+
+        foreach(string str in splittedText)
+        {
+            string reversedStr = "";
+            for (int i = str.Length-1; i>=0; i--)
+            {
+                reversedStr += str[i];
+            }
+            reversedText += reversedStr + " ";
+        }
+        return reversedText;
+    }
+}
