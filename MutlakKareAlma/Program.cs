cb431b0930cd84502238e97acce51a0949f84531
Emre Esen <emreesnn17@gmail.com>
2025-02-26 04:56:21 +0300

    Mutlak Kare Alma projesi tamamlandı.

---
+++ MutlakKareAlma/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] splittedInput = GetUserInput();
+        Calculate(splittedInput);
+    }
+
+    static string[] GetUserInput()
+    {
+        Console.WriteLine("Boşluk bırakarak istediğiniz kadar sayı girin: ");
+        string input = Console.ReadLine();
+        string[] splittedInput = input.Split(' ');
+
+        return splittedInput;
+    }
+
+    static void Calculate(string[] input)
+    {
+        int resultMin = 0;
+        int resultMax = 0;
+        foreach(string str in input)
+        {
+            int num = Convert.ToInt32(str);
+
+            if (num <= 67)
+            {
+                resultMin += 67 - num;
+            }
+            else
+            {
+                resultMax += (num - 67) * (num - 67);
+            }
+        }
+        Console.WriteLine(resultMin + " " + resultMax);
+    }
+}
+
