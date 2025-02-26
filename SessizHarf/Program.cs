4f2268c7c1c14df7e260e453a7a23374af553a1d
Emre Esen <emreesnn17@gmail.com>
2025-02-26 05:10:47 +0300

    Sessiz Harf projesi tamamlandı.

---
+++ SessizHarf/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Bir cümle giriniz:");
+        string input = Console.ReadLine();
+        string[] words = input.Split(' ');
+
+        foreach (string word in words)
+        {
+            Console.Write(SessizHarfKontrol(word) + " ");
+        }
+
+        Console.WriteLine();
+    }
+
+    static bool SessizHarfKontrol(string word)
+    {
+        string sesli = "aeıioöuüAEIİOÖUÜ";
+
+        for (int i = 0; i < word.Length - 1; i++)
+        {
+            if (!sesli.Contains(word[i]) && !sesli.Contains(word[i + 1]))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
